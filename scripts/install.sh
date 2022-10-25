1630a49da40a6bc2c9fdc87812846e403a72fb48
federicodimarzo <federico.dimarzo@bdsound.com>
2022-10-25 22:50:45 +0200

    eneabling pip install

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install the scripts in the system
-script_dir=$(realpath $(dirname -- "$0"))
-script_path="$script_dir/recursive_filepaths.sh"  
-chmod +x "$script_path"
-sudo cp "$script_path" "/bin/recursive_filepaths" || exit -1
-echo "recursive_filepaths script installed"
