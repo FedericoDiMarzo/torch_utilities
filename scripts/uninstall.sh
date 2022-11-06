ffe021fc8937806ddbd673254d1000a28860bf29
federicodimarzo <federico.dimarzo@bdsound.com>
2022-11-06 20:35:31 +0100

    removing old script

--- scripts/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# uninstall the scripts in the system
-script_dir=$(realpath $(dirname -- "$0"))
-script_path="/bin/recursive_filepaths"  
-sudo rm "$script_path" || exit -1
-echo "recursive_filepaths script uninstalled"
