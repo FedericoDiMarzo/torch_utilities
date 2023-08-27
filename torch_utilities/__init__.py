86b07373b0f394a19aca1e1c2b243171e1ebb999
federicodimarzo <federico.dimarzo@bdsound.com>
2023-08-28 00:07:13 +0200

    partial imports

--- torch_utilities/__init__.py
+++ torch_utilities/__init__.py
@@ -3,3 +3,8 @@ from torch_utilities.audio import *
 from torch_utilities.common import *
 from torch_utilities.pytorch import *
 from torch_utilities.data_loading import *
+import torch_utilities.augmentation
+import torch_utilities.features
+import torch_utilities.metrics
+import torch_utilities.modules
+import torch_utilities.losses
